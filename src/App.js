140fe7dcead1f84ca210f52412b04171778da280
Tusar <tusarahmed141@gmail.com>
2023-01-14 22:04:58 +0600

    Update DB.json file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const App = () => {
+  const [arrivals, setArrivals] = useState([]);
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/Tusar78/Furn/main/db.json")
+      .then((res) => res.json())
+      .then((data) => setArrivals(data));
+  }, []);
+  console.log('Hello world');
   return (
     <>
+      <h2>{arrivals.length}</h2>
     </>
   );
 };
