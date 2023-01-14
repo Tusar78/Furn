e72360f76be5ca8642716e10c8371c5beb313b4c
Tusar <tusarahmed141@gmail.com>
2023-01-14 12:23:38 +0600

    Install tailwindcss

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='p-8 bg-fuchsia-100 text-fuchsia-600'>
+      Peace of mind      
     </div>
   );
 };
