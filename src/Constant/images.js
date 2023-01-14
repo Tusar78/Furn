3cff6044418e9547b5aba52dc5448eb7770c91e8
Tusar <tusarahmed141@gmail.com>
2023-01-14 18:59:47 +0600

    All images export in images.js file

---
+++ src/Constant/images.js
@@ -0,0 +1,60 @@
+import slider01 from '../assets/images/slider1.png';
+import slider02 from '../assets/images/slider2.png';
+import slider03 from '../assets/images/slider3.png';
+
+import arrival1 from '../assets/images/arrivals1.png';
+import arrival2 from '../assets/images/arrivals2.png';
+import arrival3 from '../assets/images/arrivals3.png';
+import arrival4 from '../assets/images/arrivals4.png';
+import arrival5 from '../assets/images/arrivals5.png';
+import arrival6 from '../assets/images/arrivals6.png';
+import arrival7 from '../assets/images/arrivals7.png';
+import arrival8 from '../assets/images/arrivals8.png';
+
+import blog1 from '../assets/images/b1.jpg';
+import blog2 from '../assets/images/b2.jpg';
+import blog3 from '../assets/images/b3.jpg';
+
+import feature1 from '../assets/images/f1.jpg';
+import feature2 from '../assets/images/f2.jpg';
+import feature3 from '../assets/images/f3.jpg';
+import feature4 from '../assets/images/f4.jpg';
+
+import company1 from '../assets/images/c1.png';
+import company2 from '../assets/images/c2.png';
+import company3 from '../assets/images/c3.png';
+import company4 from '../assets/images/c4.png';
+import company5 from '../assets/images/c5.png';
+
+import popular1 from '../assets/images/p1.png';
+import popular2 from '../assets/images/p2.png';
+import popular3 from '../assets/images/p3.png';
+
+export default {
+  slider01,
+  slider02,
+  slider03,
+  arrival1,
+  arrival2,
+  arrival3,
+  arrival4,
+  arrival5,
+  arrival6,
+  arrival7,
+  arrival8,
+  blog1,
+  blog2,
+  blog3,
+  feature1,
+  feature2,
+  feature3,
+  feature4,
+  popular1,
+  popular2,
+  popular3,
+  company1,
+  company2,
+  company3,
+  company4,
+  company5
+}
