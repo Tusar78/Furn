4a48f662aef865b9554d9d1a496d0feacb29c999
Tusar <tusarahmed141@gmail.com>
2023-01-17 22:26:34 +0600

    Added one more component called slider

--- src/Containers/Header/Header.jsx
+++ src/Containers/Header/Header.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <>
-    
-    </>
+    <header className="header">
+      <div className="hero custom-container">
+        
+      </div>
+    </header>
   );
 };
 
