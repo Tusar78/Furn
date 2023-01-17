44f4ee308cf30933bc72e4110d970eb4c95f29be
Tusar <tusarahmed141@gmail.com>
2023-01-18 01:00:03 +0600

    Added three slider

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -57,6 +57,74 @@ const Slider = () => {
             </div>
           </div>
         </SwiperSlide>
+        <SwiperSlide className="slider__item">
+          <div className="slider__item-wrap">
+            <div className="slider__wrap-content">
+              <span className="slider__subtitle">Great Design Collection</span>
+              <h2 className="slider__title">Cloth Covered Accent Chair</h2>
+              <p className="slider__text">
+                Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit, Sed Do
+                Eiuiana Smod Tempor Ut Labore Et Dolore Magna Aliqua. Ut Enim Ad
+                Minim Veniam, Quis Nostrud Exercitation Ullamco Laboris Nisi Ut
+                Aliquip.
+              </p>
+              <div className="slider__price">
+                <span>--- $ 399.00</span>
+                <del>$ 499.00</del>
+              </div>
+              <div className="slider__buttons">
+                <a href="/home" className="slider__btn slider__btn--pill">
+                  <AiOutlinePlusCircle className="slider__btn--icon" />
+                  Add to Cart
+                </a>
+                <a href="/home" className="slider__btn slider__btn--noPill">
+                  More Info
+                </a>
+              </div>
+            </div>
+            <div className="slider__wrap-image">
+              <img
+                src={images.slider01}
+                alt="Slider Img"
+                className="slider__img"
+              />
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="slider__item">
+          <div className="slider__item-wrap">
+            <div className="slider__wrap-content">
+              <span className="slider__subtitle">Great Design Collection</span>
+              <h2 className="slider__title">Cloth Covered Accent Chair</h2>
+              <p className="slider__text">
+                Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit, Sed Do
+                Eiuiana Smod Tempor Ut Labore Et Dolore Magna Aliqua. Ut Enim Ad
+                Minim Veniam, Quis Nostrud Exercitation Ullamco Laboris Nisi Ut
+                Aliquip.
+              </p>
+              <div className="slider__price">
+                <span>--- $ 399.00</span>
+                <del>$ 499.00</del>
+              </div>
+              <div className="slider__buttons">
+                <a href="/home" className="slider__btn slider__btn--pill">
+                  <AiOutlinePlusCircle className="slider__btn--icon" />
+                  Add to Cart
+                </a>
+                <a href="/home" className="slider__btn slider__btn--noPill">
+                  More Info
+                </a>
+              </div>
+            </div>
+            <div className="slider__wrap-image">
+              <img
+                src={images.slider01}
+                alt="Slider Img"
+                className="slider__img"
+              />
+            </div>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
