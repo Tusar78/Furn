e5215375b62148b678cf3dfe5e92fa5543c35045
Tusar <tusarahmed141@gmail.com>
2023-01-17 23:19:07 +0600

    Added swipper slider in our react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Navbar } from "./Components";
+import { Header } from "./Containers";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Header />
     </>
   );
 };
--- src/Containers/Header/Header.jsx
+++ src/Containers/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Slider } from "../../Components";
 import "./Header.css";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="hero custom-container">
-        
+        <Slider />
       </div>
     </header>
   );
