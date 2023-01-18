a0352d4f2fd81d11b054a7a8c7c628d767afe86f
Tusar <tusarahmed141@gmail.com>
2023-01-18 22:50:03 +0600

    Slider almost done

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { images } from "../../Constant";
 import { AiOutlinePlusCircle } from "react-icons/ai";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { motion } from "framer-motion";
 
 // import required modules
 import { Autoplay, EffectFade, Pagination } from "swiper";
@@ -36,27 +37,48 @@ const Slider = () => {
         <SwiperSlide className="slider__item">
           <div className="slider__item-wrap">
             <div className="slider__wrap-content">
-              <span className="slider__subtitle">Great Design Collection</span>
-              <h2 className="slider__title">Cloth Covered Accent Chair</h2>
-              <p className="slider__text">
-                Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit, Sed Do
-                Eiuiana Smod Tempor Ut Labore Et Dolore Magna Aliqua. Ut Enim Ad
-                Minim Veniam, Quis Nostrud Exercitation Ullamco Laboris Nisi Ut
-                Aliquip.
-              </p>
-              <div className="slider__price">
-                <span>--- $ 399.00</span>
-                <del>$ 499.00</del>
-              </div>
-              <div className="slider__buttons">
-                <a href="/home" className="slider__btn slider__btn--pill">
-                  <AiOutlinePlusCircle className="slider__btn--icon" />
-                  Add to Cart
-                </a>
-                <a href="/home" className="slider__btn slider__btn--noPill">
-                  More Info
-                </a>
-              </div>
+              <motion.div
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  ease: "linear",
+                  duration: 0.5,
+                }}
+              >
+                <span className="slider__subtitle">
+                  Great Design Collection
+                </span>
+                <h2 className="slider__title">Cloth Covered Accent Chair </h2>
+                <p className="slider__text">
+                  Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit, Sed
+                  Do Eiuiana Smod Tempor Ut Labore Et Dolore Magna Aliqua. Ut
+                  Enim Ad Minim Veniam, Quis Nostrud Exercitation Ullamco
+                  Laboris Nisi Ut Aliquip.
+                </p>
+                <div className="slider__price">
+                  <span>--- $ 399.00</span>
+                  <del>$ 499.00</del>
+                </div>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  ease: "linear",
+                  duration: 0.5,
+                  delay: 0.3,
+                }}
+              >
+                <div className="slider__buttons">
+                  <a href="/home" className="slider__btn slider__btn--pill">
+                    <AiOutlinePlusCircle className="slider__btn--icon" />
+                    Add to Cart
+                  </a>
+                  <a href="/home" className="slider__btn slider__btn--noPill">
+                    More Info
+                  </a>
+                </div>
+              </motion.div>
             </div>
             <div className="slider__wrap-image">
               <img
@@ -70,28 +92,48 @@ const Slider = () => {
         <SwiperSlide className="slider__item">
           <div className="slider__item-wrap">
             <div className="slider__wrap-content">
-              <span className="slider__subtitle">Great Design Collection</span>
-              
-              <h2 className="slider__title">Mapple Wood Accent Chair</h2>
-              <p className="slider__text">
-                Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit, Sed Do
-                Eiuiana Smod Tempor Ut Labore Et Dolore Magna Aliqua. Ut Enim Ad
-                Minim Veniam, Quis Nostrud Exercitation Ullamco Laboris Nisi Ut
-                Aliquip.
-              </p>
-              <div className="slider__price">
-                <span>--- $ 199.00</span>
-                <del>$ 299.00</del>
-              </div>
-              <div className="slider__buttons">
-                <a href="/home" className="slider__btn slider__btn--pill">
-                  <AiOutlinePlusCircle className="slider__btn--icon" />
-                  Add to Cart
-                </a>
-                <a href="/home" className="slider__btn slider__btn--noPill">
-                  More Info
-                </a>
-              </div>
+              <motion.div
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  ease: "linear",
+                  duration: 0.5,
+                }}
+              >
+                <span className="slider__subtitle">
+                  Great Design Collection
+                </span>
+                <h2 className="slider__title">Mapple Wood Accent Chair</h2>
+                <p className="slider__text">
+                  Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit, Sed
+                  Do Eiuiana Smod Tempor Ut Labore Et Dolore Magna Aliqua. Ut
+                  Enim Ad Minim Veniam, Quis Nostrud Exercitation Ullamco
+                  Laboris Nisi Ut Aliquip.
+                </p>
+                <div className="slider__price">
+                  <span>--- $ 199.00</span>
+                  <del>$ 299.00</del>
+                </div>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  ease: "linear",
+                  duration: 0.5,
+                  delay: 0.3,
+                }}
+              >
+                <div className="slider__buttons">
+                  <a href="/home" className="slider__btn slider__btn--pill">
+                    <AiOutlinePlusCircle className="slider__btn--icon" />
+                    Add to Cart
+                  </a>
+                  <a href="/home" className="slider__btn slider__btn--noPill">
+                    More Info
+                  </a>
+                </div>
+              </motion.div>
             </div>
             <div className="slider__wrap-image">
               <img
@@ -105,27 +147,48 @@ const Slider = () => {
         <SwiperSlide className="slider__item">
           <div className="slider__item-wrap">
             <div className="slider__wrap-content">
-              <span className="slider__subtitle">Great Design Collection</span>
-              <h2 className="slider__title">Valvet Accent Arm Chair</h2>
-              <p className="slider__text">
-                Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit, Sed Do
-                Eiuiana Smod Tempor Ut Labore Et Dolore Magna Aliqua. Ut Enim Ad
-                Minim Veniam, Quis Nostrud Exercitation Ullamco Laboris Nisi Ut
-                Aliquip.
-              </p>
-              <div className="slider__price">
-                <span>--- $ 299.00</span>
-                <del>$ 399.00</del>
-              </div>
-              <div className="slider__buttons">
-                <a href="/home" className="slider__btn slider__btn--pill">
-                  <AiOutlinePlusCircle className="slider__btn--icon" />
-                  Add to Cart
-                </a>
-                <a href="/home" className="slider__btn slider__btn--noPill">
-                  More Info
-                </a>
-              </div>
+              <motion.div
+                initial={{ opacity: 0, y: 100 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  ease: "linear",
+                  duration: 0.5,
+                }}
+              >
+                <span className="slider__subtitle">
+                  Great Design Collection
+                </span>
+                <h2 className="slider__title">Valvet Accent Arm Chair</h2>
+                <p className="slider__text">
+                  Lorem Ipsum Dolor Sit Amet, Consectetur Adipisicing Elit, Sed
+                  Do Eiuiana Smod Tempor Ut Labore Et Dolore Magna Aliqua. Ut
+                  Enim Ad Minim Veniam, Quis Nostrud Exercitation Ullamco
+                  Laboris Nisi Ut Aliquip.
+                </p>
+                <div className="slider__price">
+                  <span>--- $ 299.00</span>
+                  <del>$ 399.00</del>
+                </div>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  ease: "linear",
+                  duration: 0.5,
+                  delay: 0.3,
+                }}
+              >
+                <div className="slider__buttons">
+                  <a href="/home" className="slider__btn slider__btn--pill">
+                    <AiOutlinePlusCircle className="slider__btn--icon" />
+                    Add to Cart
+                  </a>
+                  <a href="/home" className="slider__btn slider__btn--noPill">
+                    More Info
+                  </a>
+                </div>
+              </motion.div>
             </div>
             <div className="slider__wrap-image">
               <img
