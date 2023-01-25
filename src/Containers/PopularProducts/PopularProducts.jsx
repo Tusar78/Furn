991b37259e0d89beb308c24f63ad1195b3403ec0
Tusar <tusarahmed141@gmail.com>
2023-01-25 08:21:53 +0600

    Popular product section style is done

--- src/Containers/PopularProducts/PopularProducts.jsx
+++ src/Containers/PopularProducts/PopularProducts.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './PopularProducts.css';
+import "./PopularProducts.css";
 
 // Import necessary Images
 import popularProduct1 from "../../assets/images/p1.png";
@@ -12,39 +12,44 @@ const PopularProducts = () => {
       <div className="custom-container">
         <div className="popular-product__list">
           <div className="popular-product__item">
-            <div className="popular-product__img-wrap">
+            <div className="popular-product__img-wrap popular--one">
               <img
                 src={popularProduct1}
                 alt="Popular product img"
                 className="popular-product__img"
               />
             </div>
-            <h2 className="popular-product__title">Arm Chair</h2>
-            <p className="popular-product__text">
-              Nemo enim ipsam voluptatem quia volu ptas sit asperna aut odit aut
-              fugit.
-            </p>
+            <div className="popular-product__content-wrap">
+              <h2 className="popular-product__title">Arm Chair</h2>
+              <p className="popular-product__text">
+                Nemo enim ipsam voluptatem quia volu ptas sit asperna aut odit
+                aut fugit.
+              </p>
+            </div>
           </div>
           <div className="popular-product__item">
-            <div className="popular-product__img-wrap">
+            <div className="popular-product__img-wrap popular--two">
               <img
                 src={popularProduct2}
                 alt="Popular product img"
                 className="popular-product__img"
               />
             </div>
-            <h2 className="popular-product__title">
-              Latest Designed Stool and Chair
-            </h2>
-            <p className="popular-product__text">
-              Edi Ut Perspiciatis Unde Omnis Iste Natusina Error Sit Voluptatem
-              Accusantium Doloret Mque Laudantium, Totam Rem Aperiam.
-            </p>
-            <h4 className="popular-product__price">
-              Sales Start from
-              <span className="popular-product__price--number">$99.00</span>
-            </h4>
-            <button className="popular-product__btn">Discover More</button>
+            <div className="popular-product__content-wrap">
+              <h2 className="popular-product__title">
+                Latest Designed Stool and Chair
+              </h2>
+              <p className="popular-product__text product__text--two">
+                Edi Ut Perspiciatis Unde Omnis Iste Natusina Error Sit
+                Voluptatem Accusantium Doloret Mque Laudantium, Totam Rem
+                Aperiam.
+              </p>
+              <h4 className="popular-product__price">
+                Sales Start from
+                <span className="popular-product__price--number"> $99.00</span>
+              </h4>
+              <button className="popular-product__btn">Discover More</button>
+            </div>
           </div>
           <div className="popular-product__item">
             <div className="popular-product__img-wrap">
@@ -54,10 +59,13 @@ const PopularProducts = () => {
                 className="popular-product__img"
               />
             </div>
-            <h2 className="popular-product__title">Hanging Lamp</h2>
-            <p className="popular-product__text">
-              Nemo enim ipsam voluptatem quia volu ptas sit asperna aut odit aut
-            </p>
+            <div className="popular-product__content-wrao">
+              <h2 className="popular-product__title">Hanging Lamp</h2>
+              <p className="popular-product__text">
+                Nemo enim ipsam voluptatem quia volu ptas sit asperna aut odit
+                aut
+              </p>
+            </div>
           </div>
         </div>
       </div>
