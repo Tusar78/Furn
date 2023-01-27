89e25b9b8dd002ed20a808050cf67adb8950fd1a
Tusar <tusarahmed141@gmail.com>
2023-01-28 00:23:57 +0600

    Arrival item markup almost done

--- src/Components/Arrival/Arrival.jsx
+++ src/Components/Arrival/Arrival.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+// Import css file
+import './Arrival.css';
 
 const Arrival = () => {
   return (
-    <div>
-      hello World
+    <div className='arrival__item'>
+      <div className="arrival__head">
+        <div className="arrival__shadow"></div>
+        <div className="arrival__img-wrap">
+          <img src={} alt="Arrivals Img" className="arrival__img" />
+        </div>
+      </div>
+      <div className="arrival__body"></div>
     </div>
   );
 };
